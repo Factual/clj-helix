4aea54a1e9968cbab70aac5b98b9121ae08db7c8
Kyle Kingsbury <kingsbury@factual.com>
2013-07-20 09:34:58 -0700

    More screwing around with Messaging

--- src/clj_helix/message.clj
+++ src/clj_helix/message.clj
@@ -1,8 +1,10 @@
 (ns clj-helix.message
   (:import (org.apache.helix ClusterMessagingService
-                             Criteria)
+                             Criteria
+                             HelixManager)
            (org.apache.helix.model Message)
-           (org.apache.helix.messaging AsyncCallback)))
+           (org.apache.helix.messaging AsyncCallback
+                                       CriteriaEvaluator)))
 
 ;; Unfinished
 
@@ -21,16 +23,19 @@
    :self-excluded?
    :session-specific?}"
   [criteria]
-  (let [c (Criteria.)]
-    (doseq [[k v] criteria]
-      (case k
-        :resource          (.setResource c v)
-        :partition         (.setPartition c v)
-        :partition-state   (.setPartitionState c v)
-        :instance-name     (.setInstanceName c v)
-        :self-excluded?    (.setSelfExcluded c v)
-        :session-specific? (.setSessionSpecific c v)))
-    c))
+  (doto (Criteria.)
+    (.setResource (get criteria :resource "*"))
+    (.setPartition (get criteria :partition "*"))
+    (.setPartitionState (get criteria :partition-state "*"))
+    (.setInstanceName (get criteria :instance "*"))
+    (.setSelfExcluded(get criteria :self-excluded? true))))
+
+(def evaluator (CriteriaEvaluator.))
+
+(defn matching
+  "Finds instances matching the given Criteria."
+  [^HelixManager manager criteria]
+  (.evaluateCriteria evaluator criteria manager))
 
 (defn message
   "Constructs a new Message."
--- test/clj_helix/message_test.clj
+++ test/clj_helix/message_test.clj
@@ -8,3 +8,32 @@
 
 (use-fixtures :once #(mute (%)))
 
+(defn instance [context]
+  (.. context getManager getInstanceName))
+
+(defn make-fsm [state]
+  (fsm fsm-def
+       (:offline   :slave [p m c] (swap! state assoc [(instance c) p] :slave))
+       (:slave    :master [p m c] (swap! state assoc [(instance c) p] :master))
+       (:master    :slave [p m c] (swap! state assoc [(instance c) p] :slave))
+       (:slave   :offline [p m c] (swap! state dissoc [(instance c) p]))))
+
+; not sure how this is supposed to work
+;(deftest message-test
+;  (let [state (atom {})
+;        _ (add-watch state :debug (fn [_ _ x y] (prn x y)))
+;        instance {:zookeeper "localhost:2181"
+;                  :cluster   :helix-test
+;                  :instance  {:host "localhost"}}
+;        c (controller (assoc-in instance [:instance :port] 7000))
+;
+;        instance (assoc instance :fsm (make-fsm state))
+;        p1 (participant (assoc-in instance [:instance :port] 7001))
+;        instance (assoc instance :fsm (make-fsm state))
+;        p2 (participant (assoc-in instance [:instance :port] 7002))]
+;    (try
+;      (Thread/sleep 3000)
+;      (prn (matching c (criteria {})))
+;
+;       (finally
+;         (dorun (map disconnect! [p1 p2 c]))))))
